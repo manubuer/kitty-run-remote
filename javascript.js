fccc4370187a9e5eb5e22c984e53eaa0d2a44985
Manuel <mbuergel@gmx.de>
2020-03-17 16:30:45 +0100

    still correct counter and levels missing

--- javascript.js
+++ javascript.js
@@ -1,53 +1,58 @@
-/* 
-
-// the story
-- cat needs to bring item to their human companion.
-- is stuck beyond road / river which he has to cross
-- not allowed to touch moving things (cars, dogs, humans, etc) 
-- must cross river, needs to jump on logs, boats, other floating things
-- three times succeeded? -> next level
-
-// level harder
-- more opponents
-- opponents move faster
-- floating things disappear
-- opponents on floating things
-- moving opponents bigger
-- floating things smaller
-
-*/
-
-// ===================================
-
-/*
-
-// what i need
-
-- player object 
-    - maybe chose character if enough time
-    - move functions
-    - animations that cat jumps
-
-- opponent object constructor
-    - cars, dogs, humans
-
-- floating things object constuctor
-    - logs, boats, whatever floats
 
 
-*/
-
-// canvas
 window.onload = () => {
+  let canvas = document.querySelector("canvas");
+  let ctx = canvas.getContext("2d");
+
+  let logo = new Image();
+  logo.src = "images/kitty-run-logo-trans.png";
+
+  ctx.drawImage(logo, 85, 50, 640, 360);
+
+  ctx.fillStyle = "black";
+  ctx.font = "10px Arial";
+  ctx.fillText(
+    "*This game is a dirty rip-off of Frogger. But it has cats in it, and this fact alone should legitimize its existence. There are brief instructions in the",
+    85,
+    400
+  );
+  ctx.fillText(
+    "panel on the right and you could just hit play, start the fun and skip this crap here. But you decided to read it, so here is the long version: You",
+    85,
+    410
+  );
+  ctx.fillText(
+    "have to bring 3 cats safely to their home on the top of the screen. Avoid vehicles at all cost, this will kill the cat (super bad karma). Then there is",
+    85,
+    420
+  );
+  ctx.fillText(
+    "a river, cats hate water, so use floating items to get accross. Other thing to stay clear of: Cucumbers (ewww snakes), toddlers (loud and unpredic-",
+    85,
+    430
+  );
+  ctx.fillText(
+    "tive), old people (they are actually pretty nice but tend to beso freaking slow and block your way. What else? There are also snakes (ewww snakes)",
+    85,
+    440
+  );
+  ctx.fillText(
+    "as well as a time counter. If it hits 0 before the cats are safe, you screwed up. And yes, cats do have 7 lifes, just don't ask.",
+    85,
+    450
+  );
+
   document.getElementById("start-button").onclick = () => {
-    console.log("start klicked");
     start();
   };
 
   // images load & create variables
 
-  let canvas = document.querySelector("canvas");
-  let ctx = canvas.getContext("2d");
+  let background = new Image();
+  background.src = "images/canvas-background.png";
+
+  let waves = new Image();
+  waves.src = "images/wave.png";
 
   let cat = new Image();
   cat.src = "images/cat-head-transp.png";
@@ -92,6 +97,11 @@ window.onload = () => {
   let crocodile = new Image();
   crocodile.src = "images/right size/021-crocodile.png";
 
+  // audio 
+
+  let step = new Audio();
+  step.src = "audio/Frog.aiff"
+
   // frame counter & obstacle array
 
   let obstacleArray = [];
@@ -105,7 +115,9 @@ window.onload = () => {
     height: 28,
     width: 30,
     image: cat,
-    lifes: 3,
+    lifes: 4,
+    time: 40,
+    catsSaved: 0,
     gameOver: false,
 
     moveUp: function() {
@@ -134,8 +146,6 @@ window.onload = () => {
 
     update: function() {
       ctx.drawImage(player.image, player.x, player.y, 30, 30);
-      // ctx.strokeStyle = "red";
-      // ctx.strokeRect(player.x, player.y, player.width, player.height);
     },
 
     top: function() {
@@ -173,29 +183,31 @@ window.onload = () => {
     },
 
     die: function() {
-
+      
       this.lifes--;
 
+      document.getElementById("lifesId").innerText = this.lifes;
+
       if (this.lifes > 0) {
-        console.log(`You have ${this.lifes} lifes left`);
         this.x = 360;
         this.y = 570;
-        
       } else {
         this.gameOver = true;
       }
     },
 
     reset: function() {
-        this.x = 360;
-        this.y = 570;
-        this.gameOver = false;
-        this.lifes = 3
 
+      this.x = 360;
+      this.y = 570;
+      this.gameOver = false;
     },
   };
 
-  // vehicles class and cosntructor
+  document.getElementById("timeId").innerText = player.time;
+  document.getElementById("lifesId").innerText = player.lifes;
+
+  // vehicles class and constructor
 
   class Vehicles {
     constructor(posX, posY, img, speed) {
@@ -279,58 +291,145 @@ window.onload = () => {
     }
   };
 
+  // homes objects
+
+  let home1 = {
+    x: 180,
+    y: 0,
+    width: 60,
+    height: 30,
+    img: cat,
+    occupied: false,
+
+    left: function() {
+      return this.x + 5;
+    },
+
+    right: function() {
+      return this.x + this.width - 5;
+    },
+
+    top: function() {
+      return this.y;
+    },
+
+    bottom: function() {
+      return this.y + this.height - 5;
+    }
+  };
+
+  let home2 = {
+    x: 390,
+    y: 0,
+    width: 60,
+    height: 30,
+    img: cat,
+    occupied: false,
+
+    left: function() {
+      return this.x + 5;
+    },
+
+    right: function() {
+      return this.x + this.width - 5;
+    },
+
+    top: function() {
+      return this.y;
+    },
+
+    bottom: function() {
+      return this.y + this.height - 5;
+    }
+  };
+
+  let home3 = {
+    x: 600,
+    y: 0,
+    width: 60,
+    height: 30,
+    img: cat,
+    occupied: false,
+
+    left: function() {
+      return this.x + 5;
+    },
+
+    right: function() {
+      return this.x + this.width - 5;
+    },
+
+    top: function() {
+      return this.y;
+    },
+
+    bottom: function() {
+      return this.y + this.height - 5;
+    }
+  };
+
   // start function
 
   function start() {
+    
     draw();
+
+  // counter
+
+    let counter = setInterval(interval => {
+      player.time--;
+      if (player.time <= 0 || player.lifes === 0) {
+        clearInterval(counter);
+      }
+      document.getElementById("timeId").innerText = player.time;
+    }, 1000);
   }
 
   // draw function
   function draw() {
+
     if (player.gameOver == false) {
+
       frameCounter++;
 
-      //white background aka clear screen
-      ctx.fillStyle = "white";
-      ctx.fillRect(0, 0, 810, 600);
+      ctx.drawImage(background, 0, 0, 810, 600);
+      ctx.drawImage(waves, 20, 70, 32, 32);
 
-      //street
-      ctx.fillStyle = "black";
-      ctx.fillRect(0, 330, 810, 210);
-
-      ctx.fillStyle = "white";
-      ctx.fillRect(0, 420, 810, 30);
-
-      ctx.strokeStyle = "white";
-      ctx.setLineDash([5, 15]);
-
-      ctx.moveTo(0, 360);
-      ctx.lineTo(810, 360);
-      ctx.moveTo(0, 390);
-      ctx.lineTo(810, 390);
-
-      ctx.moveTo(0, 480);
-      ctx.lineTo(810, 480);
-      ctx.moveTo(0, 510);
-      ctx.lineTo(810, 510);
-
-      ctx.stroke();
-
-      //river
-      ctx.fillStyle = "black";
-      ctx.fillRect(0, 60, 810, 180);
-
-      // home
-      ctx.drawImage(home, 180, 0, 30, 30);
-      ctx.drawImage(home, 390, 0, 30, 30);
-      ctx.drawImage(home, 600, 0, 30, 30);
+      ctx.drawImage(home, 180, 0, 28, 28);
+      ctx.drawImage(home, 390, 0, 28, 28);
+      ctx.drawImage(home, 600, 0, 28, 28);
 
       ctx.strokeStyle = "black";
-      ctx.strokeRect(210, 0, 30, 30);
-      ctx.strokeRect(420, 0, 30, 30);
-      ctx.strokeRect(630, 0, 30, 30);
+      ctx.strokeRect(210, 0, 28, 28);
+      ctx.strokeRect(420, 0, 28, 28);
+      ctx.strokeRect(630, 0, 28, 28);
+
+      if (home1.occupied === true) {
+        ctx.drawImage(home1.img, home1.left() + 25, home1.top(), 30, 30);
+      }
+
+      if (home2.occupied === true) {
+        ctx.drawImage(home2.img, home2.left() + 25, home2.top(), 30, 30);
+      }
+
+      if (home3.occupied === true) {
+        ctx.drawImage(home3.img, home3.left() + 25, home3.top(), 30, 30);
+      }
 
       player.update();
+
+      // at home
+
+      let homesArray = [home1, home2, home3];
+
+      homesArray.forEach(function(home) {
+        if (player.crashWith(home) && home.occupied === false) {
+          player.catsSaved++;
+          home.occupied = true;
+          player.reset();
+        }
+      });
+
 
       // obstacle array update & remove
 
@@ -367,13 +466,11 @@ window.onload = () => {
       // draw moving things
 
       if (frameCounter % 120 === 0) {
-        console.log(obstacleArray.length);
 
         // on river
         obstacleArray.push(new Floats(canvas.width, 60, wood, 2));
         obstacleArray.push(new Floats(canvas.width, 90, box, 6));
         obstacleArray.push(new Floats(0, 120, luftLR, -3));
-        obstacleArray.push(new Floats(-150, 120, crocodile, -3));
         obstacleArray.push(new Floats(canvas.width, 150, wood, 4));
         obstacleArray.push(new Floats(canvas.width, 180, box, 3));
         obstacleArray.push(new Floats(0, 210, luftLR, -5));
@@ -391,22 +488,37 @@ window.onload = () => {
         obstacleArray.push(new Vehicles(canvas.width, 510, deliveryTruck, 6));
       }
 
-      setTimeout(function() {
-        window.requestAnimationFrame(draw);
-      }, 1000 / 24);
-    } 
-    
-    // game over screen
+      // setTimeout(function() {
+      window.requestAnimationFrame(draw);
+      // }, 1000 / 24);
+    }
+
+    // game over
     else {
       ctx.fillStyle = "white";
       ctx.fillRect(0, 0, 810, 600);
-      ctx.font = "60px Arial"
-      ctx.fillStyle = "black"
-      ctx.fillText("Game Over", 250, 300)
+      ctx.font = "60px Arial";
+      ctx.fillStyle = "black";
+      ctx.fillText("Game Over*", 250, 300);
 
-      player.reset()
+      ctx.font = "10px Arial";
+      ctx.fillText("*You suck! - The Cats", 480, 330);
+
+      player.reset();
     }
 
+    // game win
+    if (player.catsSaved === 3) {
+
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, 810, 600);
+      ctx.font = "60px Arial";
+      ctx.fillStyle = "black";
+      ctx.fillText("Meow, you win!*", 180, 300);
+
+      ctx.font = "10px Arial";
+      ctx.fillText("*We think your're great - The Cats", 430, 330);
+  }
 
     // keys
     document.onkeydown = function(e) {
